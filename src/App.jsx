eb4f7812dd034fb5f0f39752a8e6bf19fa919a97
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-19 21:12:24 +0800

    List Rendingring with Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,72 @@
 import React, { useState } from "react";
-
-import { IoIosEye, IoIosEyeOff } from "react-icons/io";
+import { GrAddCircle } from "react-icons/gr";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [user, setUsers] = useState({
-    username: "",
-    password: "",
-  });
-  const [showPass, setShowPass] = useState(true);
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setUsers((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-  const login = (e) => {
-    e.preventDefault();
-    if (user.username === "Rhogenn" && user.password === "Saingga") {
-      setIsLogin(true);
-      setUsers({
-        username: "",
-        password: "",
-      });
-    }
-  };
-  const logout = () => {
-    if (isLogin) {
-      setIsLogin(!isLogin);
-    }
-  };
+  const [todos, setTodos] = useState([
+    "Studying Javascript",
+    "Studying React",
+    "Coding or Programming",
+    "Making a coffe",
+    "Making a food",
+  ]);
+  const [newTodo, setNewTodo] = useState("");
 
-  const handleHidePassword = () => {
-    setShowPass(!showPass);
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+  const addTodo = () => {
+    if (newTodo !== "") {
+      const updatedTodo = [...todos];
+      updatedTodo.push(newTodo);
+      setTodos(updatedTodo);
+
+      setNewTodo("");
+    } else {
+      alert("Please Filled!");
+    }
+  };
+  const enterAddTodo = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
   };
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="">
-        <h1 className="mb-5 text-3xl text-center">Login Form</h1>
-        {isLogin ? (
-          <p className="mb-2.5">Welcome, User!</p>
-        ) : (
-          <p>Please Login!</p>
-        )}
-        <form action="" onSubmit={login} className="flex flex-col gap-3">
-          <label htmlFor="">Username</label>
+        <div className="flex justify-between px-2.5 py-1 text-xl border rounded-xl mb-7">
           <input
             type="text"
-            className="border rounded-xl outline-none px-2"
-            name="username"
-            value={user.username}
-            onChange={handleOnChange}
+            placeholder="Add Todo"
+            className="outline-none"
+            name="newTodo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            onKeyDown={enterAddTodo}
           />
-          <label htmlFor="">Password</label>
-          <div className="flex justify-between border rounded-xl px-2">
-            <input
-              type={showPass ? "password" : "text"}
-              className="outline-none "
-              name="password"
-              value={user.password}
-              onChange={handleOnChange}
-            />
-            {showPass ? (
-              <IoIosEye
-                size={30}
-                className="cursor-pointer"
-                onClick={handleHidePassword}
-              />
-            ) : (
-              <IoIosEyeOff
-                size={30}
-                className="cursor-pointer"
-                onClick={handleHidePassword}
-              />
-            )}
-          </div>
-          <button
-            type="submit"
-            onClick={logout}
-            className="bg-blue-500 hover:bg-blue-400 text-white hover:text-black text-2xl cursor-pointer rounded-xl"
-          >
-            {isLogin ? "Logout" : "Login"}
-          </button>
-        </form>
+          <GrAddCircle
+            size={30}
+            className="text-blue-500 hover:text-blue-400 cursor-pointer duration-150"
+            onClick={addTodo}
+          />
+        </div>
+        {todos.length > 0
+          ? todos.map((todo, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-between gap-5"
+              >
+                <span className="text-2xl">{todo}</span>
+                <RiDeleteBin6Fill
+                  size={24}
+                  className="cursor-pointer hover:text-red-500"
+                  onClick={() => deleteTodo(index)}
+                />
+              </div>
+            ))
+          : "No Result Found"}
       </div>
     </div>
   );
